c0d2dc52f99a4842b11990782667c55ed806e6d8
Rohan Patel <rpatel449@gatech.edu>
2021-10-21 22:03:39 -0400

    Working Newmark Integrator

--- NewmarkBeta.py
+++ NewmarkBeta.py
@@ -9,55 +9,49 @@ import numpy as np
 import matplotlib.pylab as plt
 
 class Newmark():
-    def __init__(self, t, x0, xdot0, u, m1, c1, k1, beta = 0.5, gamma = 0.25) :
+    def __init__(self, t, x0, xdot0, u, m1, c1, k1, beta = 0.25, gamma = 0.5) :
         self.t = t
         self.M = m1
         self.C = c1
         self.K = k1
         self.u = u
-        #print("u ", u)
         self.x0 = x0
         self.xdot0 = xdot0
         self.N = len(t)
         self.beta = beta
         self.gamma = gamma
         self.x = np.zeros((len(t),3))
-        
         self.xdot = np.zeros((len(t),3))
         self.xddot = np.zeros((len(t),3))
+        #Need to apply ICs, but since they're 0, can ignore for now
         return
     
     def forward_integration(self):
         dt = self.t[1]-self.t[0]
         
-        A = (1/(self.beta*dt**2))*self.M + (self.gamma/(self.beta*dt))*self.C+self.K
+        A = (1/(self.beta*(dt**2)))*self.M + (self.gamma/(self.beta*dt))*self.C + self.K
         invA = np.linalg.inv(A)
-        #print(np.linalg.inv(self.M) * (self.u[:,0]-self.C*self.xdot[0,:]-self.K*self.x[0,:]))
-        #print(np.dot(self.C,self.xdot[0,:]))
+
         self.xddot[0,:] = np.dot(np.linalg.inv(self.M),(self.u[:,0]-np.dot(self.C,self.xdot[0,:])-np.dot(self.K,self.x[0,:])))
-        
-        for i in range(0,self.N):
-            B = self.u[i+1] + self.M*((1/(self.beta*dt**2))*
-                                      self.x[i] + (1/(self.beta*dt))*self.xdot[i] +
-                                      (1/(2*self.beta)-1)*self.xddot[i] + self.C*((self.gamma/(self.beta*dt))*self.x[i] + 
-                                        (self.gamma/self.beta-1) * self.xdot[i] + (self.gamma/self.beta-2) * (dt/2) * 
-                                        self.xddot[i]))
-            self.x[i+1] = invA*B
+        for i in range(0,self.N-1):
+            B = self.u[:,i+1] + self.M @ ((1/(self.beta*dt**2))*
+                                      self.x[i,:] + (1/(self.beta*dt))*self.xdot[i,:] +
+                                      (1/(2*self.beta)-1)*self.xddot[i,:]) + self.C @ ((self.gamma/(self.beta*dt))*self.x[i,:] + 
+                                        (self.gamma/self.beta-1) * self.xdot[i,:] + (self.gamma/self.beta-2) * (dt/2) * 
+                                        self.xddot[i,:])
+            self.x[i+1,:] = invA@B
             
-            self.xddot[i+1] = (1/(self.beta*dt**2)) * (self.x[i+1]-self.x[i]) - (1/(self.beta*dt)) * self.xdot[i] - ((1/(2*self.beta))-1) * self.xddot[i]
-            self.xdot[i+1] = self.xdot[i] + (1-self.gamma) * dt * self.xddot[i] + self.gamma * dt * self.xddot[i+1]
-            
-            
+            self.xddot[i+1,:] = (1/(self.beta*dt**2)) * (self.x[i+1,:]-self.x[i,:]) - (1/(self.beta*dt)) * self.xdot[i,:] - ((1/(2*self.beta))-1) * self.xddot[i]
+            self.xdot[i+1,:] = self.xdot[i,:] + (1-self.gamma) * dt * self.xddot[i,:] + self.gamma * dt * self.xddot[i+1,:]
             
         return self.x
 
 
-t = np.linspace(0, 3.0 , 50)
+t = np.linspace(0, 3.0 , 601)
 x0 = np.array([0,0,0])
 xdot0 = np.array([0,0,0])
 
 u = np.zeros((3,len(t)))
-# print("u = ", u.dtype())
 pulse  =  np.zeros(len(t))
 omega = np.pi/.29
 u[2,:] = 50*np.sin(omega*t)
@@ -75,7 +69,8 @@ c1 = 3e-2*k1
 newmark = Newmark(t, x0, xdot0, u, m1, c1, k1)  
 
 x = newmark.forward_integration()
-
-fig, ax = plt.subplots(1, 1)
-ax.plot(t, x[:,1])
+fig, ax = plt.subplots(1)
+ax.plot(t, x[:,:])
+plt.xlabel('time (s)')
+plt.ylabel('displacement (m)')
 plt.show()
