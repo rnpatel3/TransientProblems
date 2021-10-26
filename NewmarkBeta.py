aee1527ca6f492bd809e6abb2d0cf33213f91833
Patel <rpatel449@ae-smdo-405015.ae.gatech.edu>
2021-10-26 14:43:57 -0400

    Initial TACS integration

--- NewmarkBeta.py
+++ NewmarkBeta.py
@@ -9,6 +9,99 @@ Created on Thu Oct 21 12:09:07 2021
 import numpy as np
 import matplotlib.pylab as plt
 
+from mpi4py import MPI
+from tacs import TACS, elements, constitutive, functions
+
+comm = MPI.COMM_WORLD
+
+# Create the stiffness object
+props = constitutive.MaterialProperties(rho=2570.0, E=70e9, nu=0.3, ys=350e6)
+stiff = constitutive.PlaneStressConstitutive(props)
+
+# Set up the basis function
+model = elements.LinearThermoelasticity2D(stiff)
+basis = elements.QuadraticTriangleBasis()
+elem = elements.Element2D(model, basis)
+
+# Allocate the TACSCreator object
+varsPerNode = model.getVarsPerNode()
+creator = TACS.Creator(comm, varsPerNode)
+
+if comm.rank == 0:
+    # Create the elements
+    nx = 2
+    ny = 2
+    
+    # Set the nodes
+    nnodes = (2*nx+1)*(2*ny+1)
+    nelems = 2*nx*ny
+    nodes = np.arange(nnodes).reshape((2*nx+1, 2*ny+1))
+    
+    conn = []
+    for j in range(ny):
+        for i in range(nx):
+            # Append the first set of nodes
+            conn.append([nodes[2*i, 2*j],
+                         nodes[2*i+2, 2*j],
+                         nodes[2*i+2, 2*j+2],
+                         nodes[2*i+1, 2*j],
+                         nodes[2*i+2, 2*j+1],
+                         nodes[2*i+1, 2*j+1]])
+            
+            # Append the second set of nodes
+            conn.append([nodes[2*i, 2*j+2],
+                         nodes[2*i, 2*j],
+                         nodes[2*i+2, 2*j+2],
+                         nodes[2*i, 2*j+1],
+                         nodes[2*i+1, 2*j+1],
+                         nodes[2*i+1, 2*j+2]])
+
+    # Set the node pointers
+    conn = np.array(conn, dtype=np.intc).flatten()
+    ptr = np.arange(0, 6*nelems+1, 6, dtype=np.intc)
+    elem_ids = np.zeros(nelems, dtype=np.intc)
+    creator.setGlobalConnectivity(nnodes, ptr, conn, elem_ids)
+
+    # Set up the boundary conditions
+    bcnodes = np.array(nodes[0,:], dtype=np.intc)
+
+    # Set the boundary condition variables
+    nbcs = 2*bcnodes.shape[0]
+    bcvars = np.zeros(nbcs, dtype=np.intc)
+    bcvars[:nbcs:2] = 0
+    bcvars[1:nbcs:2] = 1
+
+    # Set the boundary condition pointers
+    bcptr = np.arange(0, nbcs+1, 2, dtype=np.intc)
+    creator.setBoundaryConditions(bcnodes, bcvars, bcptr)
+
+    # Set the node locations
+    Xpts = np.zeros(3*nnodes)
+    x = np.linspace(0, 10, 2*nx+1)
+    y = np.linspace(0, 10, 2*nx+1)
+    for j in range(2*ny+1):
+        for i in range(2*nx+1):
+            Xpts[3*nodes[i,j]] = x[i]
+            Xpts[3*nodes[i,j]+1] = y[j]
+            
+    # Set the node locations
+    creator.setNodes(Xpts)
+
+# Set the elements
+elements = [ elem ]
+creator.setElements(elements)
+
+# Create the tacs assembler object
+assembler = creator.createTACS()
+
+res = assembler.createVec()
+ans = assembler.createVec()
+mat = assembler.createSchurMat()
+
+# Create the preconditioner for the corresponding matrix
+pc = TACS.Pc(mat)
+
+
 class Assembler():
     def __init__(self, M, C, K):
         self.u = np.zeros((3,1))
@@ -72,18 +165,22 @@ class Newmark():
 
     def forward_integration(self):
         dt =  self.t[1] - self.t[0]
-        assembler = Assembler(self.M, self.C, self.K)
+        #assembler = Assembler(self.M, self.C, self.K)
         res = np.zeros((3,1))
         J = np.zeros((3, 3))
 
         for i in range(0, self.N-1):
-            u = np.ones((1,3)) #Some estimate of u[i+1]
+            #u = np.ones((1,3)) #Some estimate of u[i+1]
+            u = assembler.createVec()
+            udot = assembler.createVec()
+            uddot = assembler.createVec()
             uddot = (u - self.x[i,:])/(self.beta*dt**2) - self.xdot[i,:]/(self.beta*dt) - (1/(2*self.beta) - 1)* self.xddot[i,:] #Eq 5.42 based on this estimate
             udot = self.gamma*(u - self.x[i,:])/(self.beta*dt) + self.xdot[i,:]*(1-self.gamma/self.beta) + dt*self.xddot[i,:]*(1-self.gamma/(2*self.beta)) #Eq. 5.43 based on estimate
             force = np.reshape(self.u[:,i+1], (1,3))
 
             for j in range(self.newton_iters):
-                assembler.setVariables(u, udot, uddot)
+                u = assembler.createVec(u)
+                
 
                 tacs_alpha = 1.0
                 tacs_beta = self.gamma/(self.beta*dt) # Eq 5.43
