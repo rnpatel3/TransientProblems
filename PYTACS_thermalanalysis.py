3a22551cb60a8cde8d3ccffb09f1e3b0c7fad684
Rohan Patel <rohan.patel2@outlook.com>
2022-03-18 12:53:13 -0400

    End Shortening Case
    -End shortening edge load working for mesh:stiffpanel4_coarse
    -Added relevant bdf/dat files for meshes

--- PYTACS_thermalanalysis.py
+++ PYTACS_thermalanalysis.py
@@ -73,7 +73,7 @@ def elemCallBack(dvNum, compID, compDescript, elemDescripts, globalDVs, **kwargs
     for elemDescript in elemDescripts:
         if elemDescript in ['CQUAD4', 'CQUADR']:
             #basis = elements.LinearQuadBasis()
-            elem = elements.Quad4ThermalShell(transform, con)
+            elem = elements.Quad4NonlinearThermalShell(transform, con)
         elif elemDescript in ['CTRIA3', 'CTRIAR']:
             basis = elements.LinearTriangleBasis()
         else:
@@ -91,7 +91,7 @@ transientOptions = {'printlevel':1}
 # Setup problems
 # Create a transient problem that will represent time varying convection
 num_steps = 21
-transientProb = FEAAssembler.createTransientProblem('MechIncrLoad', tInit=0.0, tFinal=2.0, numSteps=num_steps, options=transientOptions)
+transientProb = FEAAssembler.createTransientProblem('NonlinMechIncrLoadEdge', tInit=0.0, tFinal=2.0, numSteps=num_steps, options=transientOptions)
 # Create a static problem that will represent the steady state solution
 #staticProb = FEAAssembler.createStaticProblem(name='SteadyState')
 # Add both problems to a list
@@ -113,9 +113,11 @@ fhz = 1.0
 for nID in bdfInfo.nodes:
     nIDs.append(nID)
 
-nID_edge = [9,10,11,12,13,54,55,56,57,58,211,212,213,214,
-            238,239,240,241,526,527,528,529,553,554,555,
-            556,726,727,728,729,922,923,924,1094,1095,1096,1097]
+#nID_edge = [9,10,11,12,13,54,55,56,57,58,211,212,213,214,
+#            238,239,240,241,526,527,528,529,553,554,555,
+#            556,726,727,728,729,922,923,924,1094,1095,1096,1097]
+
+nID_edge = [191,371,506,686,825,826,827,828,910,911,912,994,995,996,1078,1079,1080,1206,1210,1211,1212]
 
 
 #Trying to add a slight initial geom imperfection
@@ -123,7 +125,7 @@ nID_edge = [9,10,11,12,13,54,55,56,57,58,211,212,213,214,
 X = transientProb.getNodes()
 Xpts = X
 Lz = 3.0
-Xpts[1::3] += 0.05*np.sin(np.pi*Xpts[2::3]/Lz)
+Xpts[1::3] += 0.1*np.sin(np.pi*Xpts[2::3]/Lz)
 transientProb.setNodes(X)
 
 #Adding dynamic loading
@@ -169,7 +171,7 @@ if comm.rank == 0:
     y = np.zeros((len(q), num_steps-1))
 
     #Start placing values into x and y vecs
-    for j in range(20):
+    for j in range(num_steps-1):
         if j > 0:
             y[:,j] = q
         t, q, qdot, qddot = problem.getVariables(j) #Returns numpy array
